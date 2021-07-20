73b138b8f733ea362bf1171ad4c577a3d5c8c686
leeseojune <sung07288346@gmail.com>
2021-07-20 22:18:58 +0900

    init

---
+++ src/test/java/com/dsm/pick/temp_server/TempServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.pick.temp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
